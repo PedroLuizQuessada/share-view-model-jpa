39ddbcda3ecab3bd036dac7c86b35965ade2f148
Pedro Quessada <pedroluiz.quessada@gmail.com>
2025-12-31 10:26:13 -0300

    fix: corregindo criação de listas não modificáveis

--- src/main/java/mappers/ClassJpaDtoMapper.java
+++ src/main/java/mappers/ClassJpaDtoMapper.java
@@ -4,6 +4,7 @@ import models.ClassJpa;
 import dtos.ClassDto;
 
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class ClassJpaDtoMapper {
 
@@ -12,16 +13,16 @@ public class ClassJpaDtoMapper {
     public static ClassJpa toClassJpa(ClassDto classDto) {
         return new ClassJpa(classDto.id(),
                 Objects.isNull(classDto.course()) ? null : CourseJpaDtoMapper.toCourseJpa(classDto.course()),
-                Objects.isNull(classDto.students()) ? null : classDto.students().stream().map(UserJpaDtoMapper::toUserJpa).toList(),
-                Objects.isNull(classDto.teachers()) ? null : classDto.teachers().stream().map(UserJpaDtoMapper::toUserJpa).toList(),
+                Objects.isNull(classDto.students()) ? null : classDto.students().stream().map(UserJpaDtoMapper::toUserJpa).collect(Collectors.toList()),
+                Objects.isNull(classDto.teachers()) ? null : classDto.teachers().stream().map(UserJpaDtoMapper::toUserJpa).collect(Collectors.toList()),
                 new java.sql.Date(classDto.startDate().getTime()));
     }
 
     public static ClassDto toClassDto(ClassJpa classJpa) {
         return new ClassDto(classJpa.getId(),
                 Objects.isNull(classJpa.getCourseJpa()) ? null : CourseJpaDtoMapper.toCourseDto(classJpa.getCourseJpa()),
-                Objects.isNull(classJpa.getStudentsJpa()) ? null : classJpa.getStudentsJpa().stream().map(UserJpaDtoMapper::toUserDto).toList(),
-                Objects.isNull(classJpa.getTeachersJpa()) ? null : classJpa.getTeachersJpa().stream().map(UserJpaDtoMapper::toUserDto).toList(),
+                Objects.isNull(classJpa.getStudentsJpa()) ? null : classJpa.getStudentsJpa().stream().map(UserJpaDtoMapper::toUserDto).collect(Collectors.toList()),
+                Objects.isNull(classJpa.getTeachersJpa()) ? null : classJpa.getTeachersJpa().stream().map(UserJpaDtoMapper::toUserDto).collect(Collectors.toList()),
                 new java.util.Date(classJpa.getStartDate().getTime()));
     }
 
